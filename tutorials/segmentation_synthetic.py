69ea46e83953005584148c4a404498781773fcd3
civodlu <civodlu@gmail.com>
2019-09-19 19:25:34 -0400

    small params change for synthetic segmentation

--- tutorials/segmentation_synthetic.py
+++ tutorials/segmentation_synthetic.py
@@ -19,10 +19,8 @@ class Net(nn.Module):
 trainer = trw.train.Trainer()
 
 model, results = trainer.fit(
-    trw.train.create_default_options(num_epochs=10),
+    trw.train.create_default_options(num_epochs=15),
     inputs_fn=lambda: trw.datasets.create_fake_symbols_2d_datasset(nb_samples=1000, image_shape=[256, 256], nb_classes_at_once=1, batch_size=50),
     run_prefix='synthetic_segmentation_unet',
     model_fn=lambda options: Net(),
-    optimizers_fn=lambda datasets, model: trw.train.create_sgd_optimizers_fn(datasets=datasets, model=model, learning_rate=0.1))
-
-print('DONE')
+    optimizers_fn=lambda datasets, model: trw.train.create_sgd_optimizers_scheduler_step_lr_fn(datasets=datasets, model=model, learning_rate=0.05, step_size=50, gamma=0.3))
