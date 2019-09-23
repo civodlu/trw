79636731f4a96d9d014be9927bee2c9fe8e9a05d
civodlu <civodlu@gmail.com>
2019-09-22 21:28:35 -0400

    simplified tutorial

--- tutorials/classification_mnist_hparams.py
+++ tutorials/classification_mnist_hparams.py
@@ -1,62 +1,39 @@
 import trw
-import torch.nn as nn
-import torch.nn.functional as F
 import os
 
 
-class Net(nn.Module):
-    """
-    Defines our model for MNIST
-    """
-    def __init__(self, hparams):
-        super(Net, self).__init__()
-        
-        number_hidden = hparams.create('number_hidden', trw.hparams.DiscreteIntegrer(500, 100, 1000))
-        number_conv1_channels = hparams.create('number_conv1_channels', trw.hparams.DiscreteIntegrer(20, 5, 100))
+def create_net(hparams):
+    number_hidden = hparams.create('number_hidden', trw.hparams.DiscreteIntegrer(500, 100, 1000))
+    number_conv1_channels = hparams.create('number_conv1_channels', trw.hparams.DiscreteIntegrer(16, 4, 64))
 
-        self.number_hidden = int(number_hidden)
-        self.number_conv1_channels = int(number_conv1_channels)
-        
-        self.conv1 = nn.Conv2d(1, self.number_conv1_channels, 5, 2)
-        self.fc1 = nn.Linear(self.number_conv1_channels * 6 * 6, self.number_hidden)
-        self.fc2 = nn.Linear(self.number_hidden, 10)
-
-    def forward(self, batch):
-        # a batch should be a dictionary of features
-        x = batch['images'] / 255.0
-
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, self.number_conv1_channels * 6 * 6)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-
-        # here we create a softmax output that will use
-        # the `targets` feature as classification target
-        return {
-            'softmax': trw.train.OutputClassification(x, 'targets')
-        }
+    n = trw.simple_layers.Input([None, 1, 28, 28], 'images')
+    n = trw.simple_layers.Conv2d(n, out_channels=number_conv1_channels, kernel_size=5, stride=2)
+    n = trw.simple_layers.ReLU(n)
+    n = trw.simple_layers.MaxPool2d(n, 2, 2)
+    n = trw.simple_layers.Flatten(n)
+    n = trw.simple_layers.Linear(n, number_hidden)
+    n = trw.simple_layers.ReLU(n)
+    n = trw.simple_layers.Linear(n, 10)
+    n = trw.simple_layers.OutputClassification(n, output_name='softmax', classes_name='targets')
+    return trw.simple_layers.compile_nn([n])
     
 
 def evaluate_hparams(hparams):
+    learning_rate = hparams.create('learning_rate', trw.hparams.ContinuousUniform(0.1, 1e-5, 1.0))
+
     # disable most of the reporting so that we don't end up with
     # thousands of files that are not useful for hyper-parameter
     # search
-    learning_rate = hparams.create('learning_rate', trw.hparams.ContinuousUniform(0.1, 1e-5, 1.0))
-    
     trainer = trw.train.Trainer(
         callbacks_pre_training_fn=None,
         callbacks_post_training_fn=None,
         callbacks_per_epoch_fn=lambda: [trw.train.callback_epoch_summary.CallbackEpochSummary()])
     
-    # make sure we log the hyper-parameters
-    options['model_parameters']['hyperparams'] = str(hparams)
-    
     model, results = trainer.fit(
         options,
-        inputs_fn=lambda: trw.datasets.create_mnist_datasset(),
+        inputs_fn=lambda: trw.datasets.create_mnist_datasset(normalize_0_1=True),
         run_prefix='run',
-        model_fn=lambda options: Net(hparams),
+        model_fn=lambda options: create_net(hparams),
         optimizers_fn=lambda datasets, model: trw.train.create_sgd_optimizers_fn(datasets=datasets, model=model, learning_rate=learning_rate))
     
     hparam_loss = trw.train.to_value(results['outputs']['mnist']['test']['overall_loss']['loss'])
@@ -71,9 +48,13 @@ trw.train.utils.create_or_recreate_folder(hparams_root)
 options['workflow_options']['logging_directory'] = hparams_root
 
 # run the hyper-parameter search
-random_search = trw.hparams.HyperParametersOptimizerRandomSearchLocal(evaluate_hparams_fn=evaluate_hparams, repeat=40)
+random_search = trw.hparams.HyperParametersOptimizerRandomSearchLocal(
+    evaluate_hparams_fn=evaluate_hparams,
+    repeat=40)
 random_search.optimize(hparams_root)
 
 # finally analyse the run
 hparams_report = os.path.join(hparams_root, 'report')
-trw.hparams.analyse_hyperparameters(hprams_path_pattern=hparams_root + '\hparams-*.pkl', output_path=hparams_report)
+trw.hparams.analyse_hyperparameters(
+    hprams_path_pattern=hparams_root + '\hparams-*.pkl',
+    output_path=hparams_report)
