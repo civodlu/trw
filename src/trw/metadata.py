9a16a572992b394eeb645e0686662f4764a6d22c
civodlu <civodlu@gmail.com>
2019-12-14 20:10:21 -0500

    env update

--- src/trw/metadata.py
+++ src/trw/metadata.py
@@ -5,13 +5,3 @@ __url__ = "https://github.com/civodlu/trw"
 __author__ = "Civodlu"
 __email__ = "civodlu@gmail.com"
 __license__ = "MIT"
-
-
-def sum(a, b):
-    """
-    Calculate the sum
-
-    >>> sum(1, 3)
-    4
-    """
-    return a + b
