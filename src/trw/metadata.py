0864bd2c7fb0d20f3e7020dab9b5df92ee09d34f
civodlu <civodlu@gmail.com>
2019-12-15 20:31:09 -0500

    torch 0.1.0 release

--- src/trw/metadata.py
+++ src/trw/metadata.py
@@ -1,5 +1,5 @@
 __name__ = "trw"
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __description__ = "Train, test, debug and optimize PyTorch models"
 __url__ = "https://github.com/civodlu/trw"
 __author__ = "Civodlu"
