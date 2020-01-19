98d8bca948f7f836ed98bd4eae1fe8f34b2c769e
civodlu <civodlu@gmail.com>
2020-01-18 19:56:53 -0500

    exported API

--- src/trw/layers/__init__.py
+++ src/trw/layers/__init__.py
@@ -1,7 +1,7 @@
 from .utils import div_shape
 from .flatten import Flatten, flatten
 from .denses import denses
-from .convs import ConvsBase
+from .convs import ConvsBase, ModulelWithIntermediate
 from .convs_2d import convs_2d
 from .convs_3d import convs_3d
 from .shift_scale import ShiftScale
