e7a0893436ad6f5cfc6f2b77263bb72257de2387
civodlu <civodlu@gmail.com>
2020-01-31 20:49:23 -0500

    doc configuration
    keep autoapi files to debug warnings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,6 +45,8 @@ extensions = [
 autoapi_type = 'python'
 autoapi_dirs = ['../../src/trw/']
 
+autoapi_keep_files = True
+
 doctest_global_setup  = """
 import trw
 """
@@ -157,7 +159,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
